d35c54a5e708fa1a0127b3c059de3497d9023bf2
Annie Dang <annie.dang@bravoco.io>
2018-08-11 11:39:12 +0900

    add basic UI

---
+++ Day-2/BasicUI/GetAnInput.java
@@ -0,0 +1,15 @@
+package Day_2.BasicUI;
+
+//Basic input with a dialog box.
+import javax.swing.JOptionPane;
+
+public class GetAnInput {
+	public static void main(String args[]) {
+		String name = JOptionPane.showInputDialog("What is your name?");
+		// create the message
+		String message = String.format("Welcome, %s!", name);
+
+		// display the message to welcome the user by name
+		JOptionPane.showMessageDialog(null, message);
+	} // end main
+}
