7d17fcd8eae418fae678755825e81d3847f3da04
Annie Dang <annie.dang@bravoco.io>
2018-08-11 11:20:34 +0900

    add finding biggest/smallest number

---
+++ Day-2/Homework/FindMeTheBiggest.java
@@ -0,0 +1,14 @@
+
+/*
+ * Write an application that reads five integers, determines and prints the largest and smallest integers in the group
+ */
+import java.util.Scanner;
+
+public class FindMeTheBiggest{
+    public static void main(String[] args){
+        // create a scanner to obtain input from the command window
+        Scanner input = new Scanner(System.in);
+
+     
+    }
+}
