4aca9de4af0b8851e222df6fa576129952cfbdb6
Annie Dang <annie.dang@bravoco.io>
2018-08-25 11:36:58 +0900

    make the bunny move

--- Day_4/Class&Object/ABunnyWantToSeeYou.java
+++ Day_4/Class&Object/ABunnyWantToSeeYou.java
@@ -2,25 +2,19 @@ package Day_4;
 
 import java.util.Scanner;
 
+public class ABunny {
+	  package Day_4;
+
+import java.util.Scanner;
+
 public class ABunnyWantToSeeYou {
-	   // create a scanner to obtain input from the command window
-       private static Scanner input = new Scanner(System.in);
-       private static int position = 0;
-       private static String BUNNY = 
-    		"           \\.\\ /,/\n" + 
-       		"            Y Y f\n" + 
-       		"            |. .|\n" + 
-       		"           (\"_, l\n" + 
-       		"            ,- , \\\n" + 
-       		"           (_)(_) Y,.\n" + 
-       		"            _j _j |,'\n" + 
-       		"           (_,(__,'\\n";
-       
+	// create a scanner to obtain input from the command window
+    private static Scanner input = new Scanner(System.in);
+    private static int position = 1;
 	   public static void main(String[] args){	  
 		   System.out.println("Hi! I'm a little bunny");
-		   System.out.println(BUNNY);
+		   showBunny(1);
 
-	   
 		   int choice = 0;
 		   while (choice != -1) {
 			   System.out.println("*************************");
@@ -51,21 +45,37 @@ public class ABunnyWantToSeeYou {
 		   System.out.println("How many step?");
 		   int steps = input.nextInt();
 		   position += steps;
-		   if(position < 0 || position > 30) {
+		   if(position < 0 || position > 100) {
 			   System.out.println("Ah.... I cannot do that!!!");
 		   } else {
-			   System.out.println("OK, I will move right "+ steps +" steps!");
-			   for(int move = 0; move< position ; move ++) {
-				   System.out.print("- hop ");
-			   }
-			   System.out.println("BUNNY");
+			   System.out.println("OK, I will hop right "+ steps +" steps!");
+			   showBunny(position);
 		   }
 	   }
 	   public static void moveLeft() {
 		   System.out.println("How many step?");
 		   int steps = input.nextInt();
 		   position -= steps;
-		   System.out.println("OK, I will move left "+ steps + " steps!");
+		   if(position < 1 || position > 100) {
+			   System.out.println("Ah.... I cannot do that!!!");
+		   } else {
+			   System.out.println("OK, I will hop left "+ steps + " steps!");
+			   showBunny(position);
+		   }
+		  
 	   }
 	   
+	   public static void showBunny(int position) {
+		   System.out.printf("%" +position +"s","\\.\\ /,/\n"); 
+		   System.out.printf("%" +position +"s","Y Y f\n");
+		   System.out.printf("%" +position +"s","|. .|\n");
+		   System.out.printf("%" +position +"s","(\"_, l\n"); 
+		   System.out.printf("%" +position +"s",",- , \\\n"); 
+		   System.out.printf("%" +position +"s","(_)(_) Y,.\n"); 
+		   System.out.printf("%" +position +"s","_j _j |,'\n"); 
+		   System.out.printf("%" +position +"s","(_,(__,'\n");
+	   }
+	   
+}
+
 }
